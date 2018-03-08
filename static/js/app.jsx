c3f72291071b2e3624f1fbac38306313c1564eb7
Stefan Majer <stefan.majer@f-i-ts.de>
2018-03-08 19:59:11 +0100

    Actions with noop impl

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -3,11 +3,29 @@ class UnitsItem extends React.Component {
   render() {
     return (
       <tr>
-        <td> {this.props.Name} </td>
+        <td> <a href="#" onClick={this.showUnit(this.props.Name)} >{this.props.Name} </a> </td>
         <td> {this.props.ActiveState} </td>
+        <td>
+          <button onClick={this.stopUnit(this.props.Name)}>Stop</button>
+          <button onClick={this.startUnit(this.props.Name)}>Start</button>
+          <button onClick={this.restartUnit(this.props.Name)}>Restart</button>
+        </td>
       </tr>
     );
   }
+  showUnit = (name) => (e) => {
+    console.log('show unit:', name);
+  }
+  stopUnit = (name) => (e) => {
+    console.log('stop unit:', name);
+  }
+  startUnit = (name) => (e) => {
+    console.log('start unit:', name);
+  }
+  restartUnit = (name) => (e) => {
+    console.log('restart unit:', name);
+  }
+
 }
 
 
@@ -37,7 +55,7 @@ class UnitsList extends React.Component {
     return (
       <div>
         <table><tbody>
-          <tr><th>Name</th><th>ActiveState</th></tr>
+          <tr><th>Name</th><th>ActiveState</th><th>Action</th></tr>
           {units}
         </tbody></table>
       </div>
