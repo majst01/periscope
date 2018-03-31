7c91bbe9b42bf28edcc3dada1602eff1e9639d80
Stefan Majer <stefan.majer@f-i-ts.de>
2018-03-31 12:19:11 +0200

    Formatting

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
+
 class UnitsItem extends React.Component {
   render() {
     var buttonGroup = (
@@ -67,7 +68,7 @@ class UnitsItem extends React.Component {
       </tr>
     );
   }
-  doUnit(name, action){
+  doUnit(name, action) {
     return (e) => {
       this.props.onUnitClicked(name, action)
     }
@@ -88,7 +89,7 @@ class UnitsList extends React.Component {
   componentDidMount() {
     this.getUnits()
     this.getReadonly()
-    window.setInterval(() => {this.getUnits()}, 5000)
+    window.setInterval(() => { this.getUnits() }, 5000)
   }
 
   getUnits() {
@@ -119,16 +120,16 @@ class UnitsList extends React.Component {
     if (action == "describe") {
       console.log('describe:', name)
       this.serverRequest =
-      axios
-        .get("/journal", {
-          params: {
-            name: name
-          }
-        })
-        .then(function(response) {
-          console.log("journal:", response)
-          self.setState({ journal: response.data })
-        });
+        axios
+          .get("/journal", {
+            params: {
+              name: name
+            }
+          })
+          .then(function (response) {
+            console.log("journal:", response)
+            self.setState({ journal: response.data })
+          });
     }
     let self = this
     this.serverRequest =
@@ -139,14 +140,14 @@ class UnitsList extends React.Component {
             action: action
           }
         })
-        .then(function(response) {
+        .then(function (response) {
           console.log(response)
           self.getUnits()
         });
   }
 
   onFilter(e) {
-    this.setState({filterString: e.target.value})
+    this.setState({ filterString: e.target.value })
   }
 
   render() {
@@ -155,18 +156,18 @@ class UnitsList extends React.Component {
     }).map((unit, i) => {
       return (
         <UnitsItem key={i} Description={unit.Description}
-                           Name={unit.Name}
-                           LoadState={unit.LoadState}
-                           ActiveState={unit.ActiveState}
-                           SubState={unit.SubState}
-                           onUnitClicked={this.onUnitClicked.bind(this)}
-                           readonly={this.state.readonly.data}/>
+          Name={unit.Name}
+          LoadState={unit.LoadState}
+          ActiveState={unit.ActiveState}
+          SubState={unit.SubState}
+          onUnitClicked={this.onUnitClicked.bind(this)}
+          readonly={this.state.readonly.data} />
       );
     });
 
-    const journalRows = this.state.journal.map((j,i) => {
+    const journalRows = this.state.journal.map((j, i) => {
       return (
-              <tr key={i}><td>{ j }</td></tr>
+        <tr key={i}><td>{j}</td></tr>
       );
     });
 
@@ -184,11 +185,11 @@ class UnitsList extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <form className="form-inline my-2 my-lg-0">
               <input className="form-control mr-sm-2"
-                     type="search"
-                     placeholder="Filter Service"
-                     id="filter"
-                     value={this.state.filterString}
-                     onChange={this.onFilter.bind(this)}></input>
+                type="search"
+                placeholder="Filter Service"
+                id="filter"
+                value={this.state.filterString}
+                onChange={this.onFilter.bind(this)}></input>
             </form>
           </div>
         </nav>
@@ -199,11 +200,11 @@ class UnitsList extends React.Component {
                 <th scope="col">Description</th>
                 <th scope="col">Name</th>
                 <th scope="col">State</th>
-                { actionHeader }
+                {actionHeader}
               </tr>
             </thead>
             <tbody>
-              { units }
+              {units}
             </tbody>
           </table>
         </div>
@@ -212,7 +213,7 @@ class UnitsList extends React.Component {
           <div className="card-body">
             <table>
               <tbody>
-              { journalRows }
+                {journalRows}
               </tbody>
             </table>
           </div>
@@ -222,4 +223,4 @@ class UnitsList extends React.Component {
   }
 }
 
-ReactDOM.render( <UnitsList/>, document.querySelector("#units"));
+ReactDOM.render(<UnitsList />, document.querySelector("#units"));
